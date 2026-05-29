8bf40de6516ef96aa42a50e7e35e5c7f8029e430
sandwe <preengus@gmail.com>
2026-05-29 14:45:20 +0900

    feat: Google Analytics 설정 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Toaster } from 'sonner';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import SplashScreen from '@/components/SplashScreen';
 import { CheckCircleIcon, AlertCircleIcon } from '@/components/icons';
 import './globals.css';
@@ -54,6 +55,7 @@ export default function RootLayout({
           }}
         />
       </body>
+      <GoogleAnalytics gaId="G-EV94GWLJM2" />
     </html>
   );
 }
