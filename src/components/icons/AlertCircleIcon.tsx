ee829ebe67e9d93c5f61180bd37028102ed0b35d
sandwe <preengus@gmail.com>
2026-07-12 11:13:00 +0900

    style: AlertCircleIcon SVG 변경

--- src/components/icons/AlertCircleIcon.tsx
+++ src/components/icons/AlertCircleIcon.tsx
@@ -1,9 +1,9 @@
 export default function AlertCircleIcon() {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="12" cy="12" r="12" fill="#F59E0B" />
-      <path d="M12 7V13" stroke="white" strokeWidth="2" strokeLinecap="round" />
-      <circle cx="12" cy="16.5" r="1" fill="white" />
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" fill="#FFC852"/>
+      <path d="M12 8V12" stroke="#515468" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M12 16H12.01" stroke="#515468" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   );
 }
